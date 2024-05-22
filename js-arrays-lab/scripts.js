67785acf312a82ddf6ff67ef40d6da8b805981b1
avnms <avnmsarma@gmail.com>
2024-05-22 15:33:02 +1000

    Completed Exercise #12

--- js-arrays-lab/scripts.js
+++ js-arrays-lab/scripts.js
@@ -157,3 +157,37 @@ for (const num of nums) {
 }
 
 console.log("Exercise 11 result:", odds);
+
+/*
+Exercise 12: FizzBuzz with arrays
+
+1) Choose a method to iterate through the `nums` array. 
+
+2. As you loop, sort the numbers into new arrays based on the following rules:
+
+   - Push any number evenly divisible by 3 to an array called `fizz`.
+   - Push any number evenly divisible by 5 to an array called `buzz`.
+   - Push any number that is evenly divisible by 3 and 5 to an array called
+     `fizzbuzz`.
+
+   Note: A single number may meet more than one of the above rules. If it does,
+         it should be placed in multiple arrays. For example, the number `15`
+         will appear in the `fizz`, `buzz`, and `fizzbuzz` arrays.
+
+Complete Exercise 12 in the space below:
+*/
+
+const fizz = [];
+const buzz = [];
+const fizzbuzz = [];
+
+for (const num of nums) {
+  if (num % 3 === 0) fizz.push(num);
+  if (num % 5 === 0) buzz.push(num);
+  if (num % 3 === 0 && num % 5 === 0) fizzbuzz.push(num);
+}
+
+console.log("Exercise 12 Results:");
+console.log("  fizz:", fizz);
+console.log("  buzz:", buzz);
+console.log("  fizzbuzz:", fizzbuzz);
