5ab857e73b64a542b369b4ed676cc99a53a2f43b
avnms <avnmsarma@gmail.com>
2024-05-22 16:16:51 +1000

    Completed Exercise #3

--- js-objects-lab/scripts.js
+++ js-objects-lab/scripts.js
@@ -22,4 +22,14 @@ const game = {
 console.log(pokemon[58]);
 
 // Exercise 2
-console.log(game);
+// console.log(game);
+
+/*
+Exercise 3
+1. Add a new property to the `game` object. Let's call it "difficulty".
+2. Choose a value for "difficulty" that you think fits the game. Ex: "Easy", "Med" or "Hard". How would you assign it?
+
+
+Solve Exercise 3 here:
+*/
+game.difficulty = "Med";
