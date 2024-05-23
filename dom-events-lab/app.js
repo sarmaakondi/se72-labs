003290c6a65ea02fc7b920840c982477d40596b3
avnms <avnmsarma@gmail.com>
2024-05-23 20:07:24 +1000

    Implemented basic single digit based calculation

--- dom-events-lab/app.js
+++ dom-events-lab/app.js
@@ -1,24 +1,59 @@
 /*-------------------------------- Constants --------------------------------*/
-const calculator = document.querySelector("#calculator");
 
 /*-------------------------------- Variables --------------------------------*/
+let result = 0;
+let num1 = 0;
+let num2 = 0;
+let operator = "";
 
 /*------------------------ Cached Element References ------------------------*/
+const calculator = document.getElementById("calculator");
+const displayElement = document.querySelector(".display");
 
 /*----------------------------- Event Listeners -----------------------------*/
 
 calculator.addEventListener("click", (event) => {
   if (event.target.classList.contains("number")) {
-    console.log("Clicked Number");
+    if (operator == "") {
+      num1 = event.target.innerText;
+      displayElement.textContent = num1;
+      console.log(`num1: ${num1}`);
+    } else {
+      displayElement.textContent = "";
+      num2 = event.target.innerText;
+      displayElement.textContent = num2;
+      console.log(`num2: ${num2}`);
+    }
   }
 
   if (event.target.classList.contains("operator")) {
-    console.log("Clicked Operator");
+    operator = event.target.innerText;
+    console.log(`operator: ${operator}`);
   }
 
   if (event.target.classList.contains("equals")) {
-    console.log("Clicked Equals");
+    result = calculate(num1, num2, operator);
+    console.log(`result: ${result}`);
+    displayElement.textContent = result;
+    num1 = 0;
+    num2 = 0;
+    operator = "";
   }
 });
 
 /*-------------------------------- Functions --------------------------------*/
+const calculate = (num1, num2, operator) => {
+  let output = 0;
+
+  if (operator === "+") {
+    output = parseInt(num1) + parseInt(num2);
+  } else if (operator === "-") {
+    output = parseInt(num1) - parseInt(num2);
+  } else if (operator === "*") {
+    output = parseInt(num1) * parseInt(num2);
+  } else if (operator === "/") {
+    output = parseInt(num1) / parseInt(num2);
+  }
+
+  return output;
+};
