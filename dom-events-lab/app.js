7b79e17de46d6b354c7ce2b550c043b57b84ab36
avnms <avnmsarma@gmail.com>
2024-05-23 20:27:19 +1000

    Updating logic to handle multi-digit numbers calculation

--- dom-events-lab/app.js
+++ dom-events-lab/app.js
@@ -2,8 +2,8 @@
 
 /*-------------------------------- Variables --------------------------------*/
 let result = 0;
-let num1 = 0;
-let num2 = 0;
+let num1 = "";
+let num2 = "";
 let operator = "";
 
 /*------------------------ Cached Element References ------------------------*/
@@ -15,12 +15,12 @@ const displayElement = document.querySelector(".display");
 calculator.addEventListener("click", (event) => {
   if (event.target.classList.contains("number")) {
     if (operator == "") {
-      num1 = event.target.innerText;
+      num1 += event.target.innerText;
       displayElement.textContent = num1;
       console.log(`num1: ${num1}`);
     } else {
       displayElement.textContent = "";
-      num2 = event.target.innerText;
+      num2 += event.target.innerText;
       displayElement.textContent = num2;
       console.log(`num2: ${num2}`);
     }
@@ -35,8 +35,8 @@ calculator.addEventListener("click", (event) => {
     result = calculate(num1, num2, operator);
     console.log(`result: ${result}`);
     displayElement.textContent = result;
-    num1 = 0;
-    num2 = 0;
+    num1 = "";
+    num2 = "";
     operator = "";
   }
 });
