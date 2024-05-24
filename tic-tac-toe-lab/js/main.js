a811735a735bde7446997c6d16c1fcfb7c25ba69
avnms <avnmsarma@gmail.com>
2024-05-24 20:33:28 +1000

    Completed Exercise Steps 4 and 5

--- tic-tac-toe-lab/js/main.js
+++ tic-tac-toe-lab/js/main.js
@@ -1,4 +1,14 @@
 /*-------------------------------- Constants --------------------------------*/
+const winningCombos = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [6, 4, 2],
+];
 
 /*---------------------------- Variables (state) ----------------------------*/
 let board;
@@ -22,7 +32,27 @@ function init() {
 }
 
 function render() {
+  console.log(squareElements);
   console.log("Render function");
+  updateBoard();
+  updateMessage();
+}
+
+function updateBoard() {
+  board.forEach((element, index) => {
+    const squareElement = squareElements[index];
+    squareElement.textContent = element;
+  });
+}
+
+function updateMessage() {
+  if (winner === false && tie === false) {
+    console.log(`Player turn: ${turn}`);
+  } else if (winner === false && tie === true) {
+    console.log("It's a tie");
+  } else {
+    console.log(`The winner is: ${turn}`);
+  }
 }
 
 /*----------------------------- Event Listeners -----------------------------*/
