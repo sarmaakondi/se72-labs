4862b050e84ebfb5afacd8fc3c1fc444fd5ecbb3
avnms <avnmsarma@gmail.com>
2024-05-24 15:14:30 +1000

    Completed Exercise Steps 1, 2, and 3

--- tic-tac-toe-lab/js/main.js
+++ tic-tac-toe-lab/js/main.js
@@ -1,9 +1,31 @@
 /*-------------------------------- Constants --------------------------------*/
 
 /*---------------------------- Variables (state) ----------------------------*/
+let board;
+let turn;
+let winner;
+let tie;
+
+board = ["", "", "", "", "", "", "", "", ""];
+turn = "X";
+winner = false;
+tie = false;
 
 /*------------------------ Cached Element References ------------------------*/
+const squareElements = document.querySelectorAll(".sqr");
+const messageElement = document.getElementById("message");
 
 /*-------------------------------- Functions --------------------------------*/
+function init() {
+  console.log("Game started");
+  render();
+}
+
+function render() {
+  console.log("Render function");
+}
 
 /*----------------------------- Event Listeners -----------------------------*/
+document.addEventListener("DOMContentLoaded", function () {
+  init();
+});
