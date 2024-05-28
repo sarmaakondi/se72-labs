063037fe658420c13afcf98b8fd72764d8350c21
avnms <avnmsarma@gmail.com>
2024-05-28 16:58:49 +1000

    Completed Exercise Steps 1, 2, and 3

--- tamagotchi-lab/js/app.js
+++ tamagotchi-lab/js/app.js
@@ -1,9 +1,37 @@
 /*-------------------------------- Constants --------------------------------*/
 
 /*---------------------------- Variables (state) ----------------------------*/
+const state = {
+  boredom: 0,
+  hunger: 0,
+  sleepiness: 0,
+};
+
+let timer;
+let gameOver;
 
 /*------------------------ Cached Element References ------------------------*/
+const boredomStatElement = document.getElementById("boredom-stat");
+const hungerStatElement = document.getElementById("hunger-stat");
+const sleepinessStatElement = document.getElementById("sleepiness-stat");
+
+const playButtonElement = document.getElementById("play");
+const feedButtonElement = document.getElementById("feed");
+const sleepButtonElement = document.getElementById("sleep");
+
+const gameMessageElement = document.getElementById("message");
+
+const resetButtonElement = document.getElementById("restart");
 
 /*-------------------------------- Functions --------------------------------*/
+function init() {
+  gameOver = false;
+  timer = setInterval(runGame(), 2000);
+  render();
+}
 
+function runGame() {
+  console.log("The game is running!");
+}
 /*----------------------------- Event Listeners -----------------------------*/
+document.addEventListener("DOMContentLoaded", init);
