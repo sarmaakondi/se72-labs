4ce31211f451fb9104b33ec4ed6c43645d813bfe
avnms <avnmsarma@gmail.com>
2024-05-29 13:14:25 +1000

    Completed Exercise #5

--- array-iterator-methods-lab/script.js
+++ array-iterator-methods-lab/script.js
@@ -199,3 +199,64 @@ console.log("Exercise 4 Correct Result: ", {
   year: 1815,
   passed: 1852,
 });
+
+// Array.prototype.map()
+
+// 5. Use the Array.prototype.map() method to reformat each name in the 'people' array.
+// The goal is to convert names from "Last, First" format to "First Last" format.
+
+// Hint: Use the String.prototype.split method to separate the first and last names. You can split the string using ', ' as the separator.
+// After splitting the names, rearrange them to the "First Last" format.
+
+let firstLast = [];
+
+// Enter your solution code here:
+people.map((person) => {
+  const [lastName, firstName] = person.split(",");
+  firstLast.push(`${firstName.trim()} ${lastName.trim()}`);
+});
+
+// Check your return value:
+console.log("Exercise 5 My Result: ", firstLast);
+console.log("Exercise 5 Correct Result: ", [
+  "Carl Becker",
+  "Samuel Beckett",
+  "Mick Beddoes",
+  "Henry Beecher",
+  "Ludwig Beethoven",
+  "Menachem Begin",
+  "Hilaire Belloc",
+  "Saul Bellow",
+  "Robert Benchley",
+  "Peter Benenson",
+  "David Ben-Gurion",
+  "Walter Benjamin",
+  "Tony Benn",
+  "Chester Bennington",
+  "Leana Benson",
+  "Silas Bent",
+  "Lloyd Bentsen",
+  "Ric Berger",
+  "Ingmar Bergman",
+  "Luciano Berio",
+  "Milton Berle",
+  "Irving Berlin",
+  "Eric Berne",
+  "Sandra Bernhard",
+  "Yogi Berra",
+  "Halle Berry",
+  "Wendell Berry",
+  "Erin Bethea",
+  "Aneurin Bevan",
+  "Ken Bevel",
+  "Joseph Biden",
+  "Ambrose Bierce",
+  "Steve Biko",
+  "Josh Billings",
+  "Frank Biondo",
+  "Augustine Birrell",
+  "Elk Black",
+  "Robert Blair",
+  "Tony Blair",
+  "William Blake",
+]);
