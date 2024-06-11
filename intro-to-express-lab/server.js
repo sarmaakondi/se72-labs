9a0f36edbd10969fa46cd8c1eff66f9935aceba9
avnms <avnmsarma@gmail.com>
2024-06-11 17:57:45 +1000

    Created collectibles route

--- intro-to-express-lab/server.js
+++ intro-to-express-lab/server.js
@@ -31,6 +31,7 @@ server.get("/roll", (req, res) => {
 // positive case
 server.get("/roll/:maxValue", (req, res) => {
   const maxValue = parseInt(req.params.maxValue, 10);
+
   if (isNaN(maxValue)) {
     res.send("<h1>You must specify a number.</h1>");
   } else if (maxValue === 0) {
@@ -40,5 +41,33 @@ server.get("/roll/:maxValue", (req, res) => {
   } else {
     const diceValue = Math.floor(Math.random() * maxValue) + 1;
     res.send(`<h1>You rolled a ${diceValue}</h1>`);
+  }
+});
+
+// i want that one!
+server.get("/collectibles", (req, res) => {
+  res.send(`<h1>Correct usage is: '/collectibles/index-0-or-greater'</h1>`);
+});
+
+server.get("/collectibles/:index", (req, res) => {
+  const collectibles = [
+    { name: "shiny ball", price: 5.95 },
+    { name: "autographed picture of a dog", price: 10 },
+    { name: "vintage 1970s yogurt SOLD AS-IS", price: 0.99 },
+  ];
+  const index = req.params.index;
+
+  if (isNaN(index)) {
+    res.send("<h1>You must specify a number.</h1>");
+  } else if (index < 0) {
+    res.send("<h1>Index cannot be negative.</h1>");
+  } else if (index > collectibles.length - 1) {
+    res.send("<h1>This item is not yet in stock. Check back soon!</h1>");
+  } else {
+    const item = collectibles[index].name;
+    const price = collectibles[index].price;
+    res.send(
+      `<h1>So, you want the ${item}? For ${price}, it can be yours!</h1>`
+    );
   }
 });
