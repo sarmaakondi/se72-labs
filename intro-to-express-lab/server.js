f855261cd8d2f7aa8031e9d6f18c1c45c3b201d7
avnms <avnmsarma@gmail.com>
2024-06-11 16:57:40 +1000

    Created root route

--- intro-to-express-lab/server.js
+++ intro-to-express-lab/server.js
@@ -6,3 +6,9 @@ const port = 3000;
 server.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
+
+// routes
+// root
+server.get("/", (req, res) => {
+  res.send("<h1>Intro to Express Lab</h1>");
+});
