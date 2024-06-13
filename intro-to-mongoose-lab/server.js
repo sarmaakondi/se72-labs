d6b224c40d5d22175288f005a7fa121471b136c9
avnms <avnmsarma@gmail.com>
2024-06-13 18:08:58 +1000

    Display welcome message and ask for operation to perform

--- intro-to-mongoose-lab/server.js
+++ intro-to-mongoose-lab/server.js
@@ -1,6 +1,17 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userPrompt = require("prompt-sync")();
+const Customer = require("./models/customer.js");
+
+const title = "Welcome to the CRM";
+const question = "What would you like to do?";
+const operations = [
+  { id: 1, description: "Create a customer" },
+  { id: 2, description: "View all customers" },
+  { id: 3, description: "Update a customer" },
+  { id: 4, description: "Delete a customer" },
+  { id: 5, description: "Quit" },
+];
 
 // dotenv initialization
 dotenv.config();
@@ -8,20 +19,32 @@ dotenv.config();
 // main function
 const connect = async () => {
   // connect to MongoDB
-  await mongoose.connect(process.env.MONGODB_URI);
-  console.log("Connected to MongoDB");
+  // await mongoose.connect(process.env.MONGODB_URI);
 
   // perform CRM operations per user request
   await performOperations();
 
   // disconnect from MongoDB
-  await mongoose.disconnect();
-  console.log("Disconnected from MongoDB");
+  // await mongoose.disconnect();
+};
+
+// CRM functions
+// display welcome message
+const displayWelcomeMessage = (title, question, operations) => {
+  console.log(title, "\n");
+  console.log(question, "\n");
+  operations.forEach((operation) => {
+    console.log(`${operation.id}. ${operation.description}`);
+  });
+  console.log();
+  const response = userPrompt("Number of action to run: ");
+  return response;
 };
 
 // handle CRM operations
 const performOperations = async () => {
-  console.log("Performing CRM operations!");
+  const actionNumber = await displayWelcomeMessage(title, question, operations);
+  console.log(actionNumber);
 };
 
 connect();
