99e2ab8239629a2208b61e4f47cb4ad753eb6188
avnms <avnmsarma@gmail.com>
2024-06-13 22:19:27 +1000

    Added create customer feature

--- intro-to-mongoose-lab/server.js
+++ intro-to-mongoose-lab/server.js
@@ -6,7 +6,13 @@ const Customer = require("./models/customer.js");
 const title = "Welcome to the CRM";
 const question = "What would you like to do?";
 const operations = [
-  { id: 1, description: "Create a customer" },
+  {
+    id: 1,
+    description: "Create a customer",
+    action: async () => {
+      await createCustomer();
+    },
+  },
   { id: 2, description: "View all customers" },
   { id: 3, description: "Update a customer" },
   { id: 4, description: "Delete a customer" },
@@ -17,15 +23,27 @@ const operations = [
 dotenv.config();
 
 // main function
-const connect = async () => {
+const main = async () => {
   // connect to MongoDB
-  // await mongoose.connect(process.env.MONGODB_URI);
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+  } catch (error) {
+    console.log("Error connecting to MongoDB: ", error);
+  }
 
   // perform CRM operations per user request
-  await performOperations();
+  try {
+    await performOperations();
+  } catch (error) {
+    console.log("Error performing user requested operation: ", error);
+  }
 
   // disconnect from MongoDB
-  // await mongoose.disconnect();
+  try {
+    await mongoose.disconnect();
+  } catch (error) {
+    console.log("Error disconnecting from MongoDB: ", error);
+  }
 };
 
 // CRM functions
@@ -37,14 +55,64 @@ const displayWelcomeMessage = (title, question, operations) => {
     console.log(`${operation.id}. ${operation.description}`);
   });
   console.log();
-  const response = userPrompt("Number of action to run: ");
-  return response;
+
+  // prompt for user response
+  let response;
+  do {
+    response = userPrompt("Number of action to run: ");
+    if (!/^\d+$/.test(response) || response <= 0 || response > 5) {
+      console.error("Invalid input! options available are from 1 to 5 only.\n");
+    }
+  } while (isNaN(response) || response <= 0 || response > 5);
+  return parseInt(response, 10);
+};
+
+// create new customer
+const createCustomer = async () => {
+  let customerName;
+  let customerAge;
+
+  // prompt for customer name
+  do {
+    customerName = userPrompt("Enter the customer name: ");
+  } while (customerName.trim() === "" || !isNaN(customerName));
+
+  // prompt for customer age
+  do {
+    customerAge = userPrompt("Enter the customer age (must be a number): ");
+    if (!/^\d+$/.test(customerAge)) {
+      console.error("Invalid age! please enter a positive whole number.");
+    }
+  } while (isNaN(customerAge) || customerAge <= 0);
+
+  // create customer data
+  const customerData = {
+    name: customerName
+      .toLowerCase()
+      .trim()
+      .split(" ")
+      .map(
+        (word) => word[0].toUpperCase() + (word.length > 1 ? word.slice(1) : "")
+      )
+      .join(" "),
+    age: parseInt(customerAge, 10),
+  };
+
+  try {
+    const customer = await Customer.create(customerData);
+    console.log("Added customer details successfully!");
+  } catch (error) {
+    console.log("Error occurred: ", error);
+  }
 };
 
 // handle CRM operations
 const performOperations = async () => {
-  const actionNumber = await displayWelcomeMessage(title, question, operations);
-  console.log(actionNumber);
+  const actionNumber = displayWelcomeMessage(title, question, operations);
+  const operation = operations.find((op) => op.id === actionNumber);
+  if (operation) {
+    await operation.action();
+  }
 };
 
-connect();
+main();
