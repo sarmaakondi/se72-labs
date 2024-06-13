5a23896a370450e794e84f344fc6d4b1fbacf6b3
avnms <avnmsarma@gmail.com>
2024-06-13 17:33:40 +1000

    Initialized DB connection and placeholder function to handle use requested operations

--- intro-to-mongoose-lab/server.js
+++ intro-to-mongoose-lab/server.js
@@ -1,1 +1,27 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 const userPrompt = require("prompt-sync")();
+
+// dotenv initialization
+dotenv.config();
+
+// main function
+const connect = async () => {
+  // connect to MongoDB
+  await mongoose.connect(process.env.MONGODB_URI);
+  console.log("Connected to MongoDB");
+
+  // perform CRM operations per user request
+  await performOperations();
+
+  // disconnect from MongoDB
+  await mongoose.disconnect();
+  console.log("Disconnected from MongoDB");
+};
+
+// handle CRM operations
+const performOperations = async () => {
+  console.log("Performing CRM operations!");
+};
+
+connect();
