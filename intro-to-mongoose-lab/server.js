45c4e82d41bb184b35f5a468d1130c3abf3d0473
avnms <avnmsarma@gmail.com>
2024-06-13 23:13:20 +1000

    Implemented logic to update existing customer details per user instruction

--- intro-to-mongoose-lab/server.js
+++ intro-to-mongoose-lab/server.js
@@ -20,7 +20,13 @@ const operations = [
       await displayCustomers();
     },
   },
-  { id: 3, description: "Update a customer" },
+  {
+    id: 3,
+    description: "Update a customer",
+    action: async () => {
+      await updateCustomer();
+    },
+  },
   {
     id: 4,
     description: "Delete a customer",
@@ -92,12 +98,12 @@ const createCustomer = async () => {
 
   // prompt for customer name
   do {
-    customerName = userPrompt("Enter the customer name: ");
+    customerName = userPrompt("Enter the customer's name: ");
   } while (customerName.trim() === "" || !isNaN(customerName));
 
   // prompt for customer age
   do {
-    customerAge = userPrompt("Enter the customer age (must be a number): ");
+    customerAge = userPrompt("Enter the customer's age (must be a number): ");
     if (!/^\d+$/.test(customerAge)) {
       console.error("Invalid age! please enter a positive whole number.");
     }
@@ -136,6 +142,73 @@ const displayCustomers = async () => {
     }
   } catch (error) {
     console.log("Error while fetching customers: ", error);
+  }
+};
+
+// update customer details
+const updateCustomer = async () => {
+  try {
+    const customers = await Customer.find();
+    console.log("Below is a list of customers:\n");
+    for (const customer of customers) {
+      console.log(
+        `id: ${customer.id} -- Name: ${customer.name}, Age: ${customer.age}`
+      );
+    }
+
+    // validate user entered customer ID
+    let customerId;
+    do {
+      customerId = userPrompt(
+        "\nCopy and paste the id of the customer you would like to delete here: "
+      );
+    } while (customerId.trim() === "");
+
+    const matchingCustomer = customers.find(
+      (customer) => customer.id === customerId
+    );
+
+    // gather new details from user
+    if (matchingCustomer) {
+      let customerNewName = userPrompt("Enter the customer's new name: ");
+      let customerNewAge = userPrompt(
+        "Enter the customer's new age (must be a number): "
+      );
+      // validate user entered details
+      if (isNaN(customerNewName) && customerNewName.trim() !== "") {
+        customerNewName = customerNewName
+          .toLowerCase()
+          .trim()
+          .split(" ")
+          .map(
+            (word) =>
+              word[0].toUpperCase() + (word.length > 1 ? word.slice(1) : "")
+          )
+          .join(" ");
+      } else {
+        customerNewName = matchingCustomer.name;
+      }
+      if (
+        !isNaN(customerNewAge) &&
+        customerNewAge > 0 &&
+        /^\d+$/.test(customerNewAge)
+      ) {
+        customerNewAge = parseInt(customerNewAge, 10);
+      } else {
+        customerNewAge = matchingCustomer.age;
+      }
+      // create customer data object with new details
+      const customerData = {
+        name: customerNewName,
+        age: customerNewAge,
+      };
+      // update customer details
+      await Customer.findByIdAndUpdate(customerId, customerData);
+    } else {
+      console.log(`Customer not found for the 'ID: ${customerId}', try again!`);
+    }
+  } catch (error) {
+    console.log("Error while updating customer details: ", error);
   }
 };
 
