07affeb3507b7a2345502f21088a3ed4f289538a
avnms <avnmsarma@gmail.com>
2024-06-15 08:24:15 +1000

    Created and validated connection to MongoDB

--- crud-app-lab/server.js
+++ crud-app-lab/server.js
@@ -2,11 +2,18 @@
 require("dotenv").config();
 
 const express = require("express");
+const mongoose = require("mongoose");
 const server = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 // middleware
 server.use(express.static("public"));
+
+// connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI);
+mongoose.connection.on("connected", () => {
+  console.log(`Connected to MongoDB: ${mongoose.connection.name}`);
+});
 
 // routes
 // root
