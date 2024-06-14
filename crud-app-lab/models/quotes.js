f32d301ed9b4cc88fc311809063e95de0c3cf225
avnms <avnmsarma@gmail.com>
2024-06-14 22:11:29 +1000

    Created quote schema and model

---
+++ crud-app-lab/models/quotes.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+// quotes schema
+const quoteSchema = new mongoose.Schema(
+  {
+    quote: { type: String, required: true },
+    author: { type: String },
+    category: { type: String },
+    upvotes: { type: Number, default: 0 },
+    downvotes: { type: Number, default: 0 },
+  },
+  { timestamps: true }
+);
+
+// quote model
+const Quote = mongoose.model("Quote", quoteSchema);
