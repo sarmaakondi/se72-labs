76ebc4c3f67c77c3ddab883a3290a96d05ff94e2
avnms <avnmsarma@gmail.com>
2024-06-15 12:13:01 +1000

    Created endpoint to add quote and store in MongoDB

--- crud-app-lab/server.js
+++ crud-app-lab/server.js
@@ -6,8 +6,12 @@ const mongoose = require("mongoose");
 const server = express();
 const port = process.env.PORT || 3000;
 
+// import models
+const Quote = require("./models/quotes");
+
 // middleware
 server.use(express.static("public"));
+server.use(express.urlencoded({ extended: false }));
 
 // connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI);
@@ -24,6 +28,48 @@ server.get("/", (req, res) => {
 // add new quote | GET
 server.get("/quotes/new", (req, res) => {
   res.render("quotes/new.ejs");
+});
+
+// store the quote in MongoDB | POST
+server.post("/quotes", async (req, res) => {
+  let quote = req.body.quote;
+  let author = req.body.author;
+  let category = req.body.category;
+
+  // validate quote body
+  if (quote.trim() === "") {
+    return;
+  } else {
+    quote = quote[0].toUpperCase() + quote.slice(1);
+  }
+
+  // validate author field and set default value
+  if (author.trim() === "") {
+    author = "anonymous";
+  } else {
+    author = author
+      .toLowerCase()
+      .trim()
+      .split(" ")
+      .map(
+        (word) => word[0].toUpperCase() + (word.length > 1 ? word.slice(1) : "")
+      )
+      .join(" ");
+  }
+
+  // validate category field and set default value
+  if (category.trim() === "") category = "uncategorized";
+
+  // create qoute data object
+  const quoteData = {
+    quote: quote,
+    author: author,
+    category: category,
+  };
+
+  // add post to MongoDB
+  await Quote.create(quoteData);
+  res.redirect("/quotes/new");
 });
 
 // server listening on specified port
