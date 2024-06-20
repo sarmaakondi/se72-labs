c57da1d2f0e4e540c6c9953689a64bdc8691f931
avnms <avnmsarma@gmail.com>
2024-06-20 20:28:42 +1000

    Created endpoint to handle add new item

--- data-lab-cookbook/controllers/foods.js
+++ data-lab-cookbook/controllers/foods.js
@@ -8,8 +8,20 @@ router.get("/", (req, res) => {
   res.render("foods/index.ejs");
 });
 
-router.get("/new", async (req, res) => {
+router.get("/new", (req, res) => {
   res.render("foods/new.ejs");
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const user = await User.findById(req.session.user._id);
+    user.pantry.push(req.body);
+    await user.save();
+    res.redirect(`/users/${user._id}/foods`);
+  } catch (error) {
+    console.log(error);
+    res.redirect("/");
+  }
 });
 
 module.exports = router;
