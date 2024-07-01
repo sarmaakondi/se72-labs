96025539a96dc1e317e645da2943ba934c666860
avnms <avnmsarma@gmail.com>
2024-07-01 16:32:22 +1000

    Completed Exercise #11

--- modern-javascript-syntax-lab/index.js
+++ modern-javascript-syntax-lab/index.js
@@ -108,3 +108,15 @@ console.log("Language setting:", LANG);
 const userSavedTheme = "dark";
 const USER_THEME = userSavedTheme || "light";
 console.log("User theme setting:", USER_THEME);
+
+/* 
+Exercise 11: Optional chaining
+Now check for `cat.age` on `adventurer`. See how it errors out? Use optional chaining in a console.log that it returns undefined instead.
+*/
+const adventurer = {
+  name: "Alice",
+};
+
+let cat = adventurer.cat?.age;
+
+console.log(cat);
