e78b0f7a41877666acc4ca0edef12142ac495bdf
avnms <avnmsarma@gmail.com>
2024-07-01 15:34:51 +1000

    Completed Exercise #2

--- modern-javascript-syntax-lab/index.js
+++ modern-javascript-syntax-lab/index.js
@@ -6,3 +6,12 @@ Create a new array where each value is multiplied by 2 and log the new array.
 const nums = [13, 87, 2, 89, 12, 4, 90, 63];
 const multipliedByTwo = nums.map((num) => num * 2);
 console.log(multipliedByTwo);
+
+/* 
+Exercise 2: Array destructuring
+Given the following array, use destructuring to pull out the first and second values and place them into variables. Log both variables.
+*/
+const pizzaToppings = ["Pineapple", "Olives", "Anchovies"];
+const [firstTopping, secondTopping] = pizzaToppings;
+console.log(firstTopping);
+console.log(secondTopping);
