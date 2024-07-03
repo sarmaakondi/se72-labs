956189562abb43d33765aa372e72259fc7d9e658
avnms <avnmsarma@gmail.com>
2024-07-03 14:56:13 +1000

    Defined the initial state for the book shelf component

--- controlled-forms-in-react-lab/src/components/BookShelf/BookShelf.jsx
+++ controlled-forms-in-react-lab/src/components/BookShelf/BookShelf.jsx
@@ -1,4 +1,17 @@
+import { useState } from "react";
+
 const BookShelf = () => {
+  const [books, setBooks] = useState([
+    {
+      title: "Fourth Wing",
+      author: "Rebecca Yarros",
+    },
+    {
+      title: "The Lio, the Witch and the Wardrobe",
+      author: "C.S. Lewis",
+    },
+  ]);
+
   return (
     <>
       <div>
