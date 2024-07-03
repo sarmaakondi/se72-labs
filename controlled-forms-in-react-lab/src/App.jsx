43f65065ce451bfb7ea8f4aeb444bf172f8baa08
avnms <avnmsarma@gmail.com>
2024-07-03 14:53:55 +1000

    Created the book shelf component

--- controlled-forms-in-react-lab/src/App.jsx
+++ controlled-forms-in-react-lab/src/App.jsx
@@ -1,9 +1,12 @@
 import "./App.css";
 
+import BookShelf from "./components/BookShelf/BookShelf";
+
 function App() {
   return (
     <>
-      <h1>Hello from React!</h1>
+      <h1>My Bookshelf</h1>
+      <BookShelf />
     </>
   );
 }
---
+++ controlled-forms-in-react-lab/src/components/BookShelf/BookShelf.jsx
@@ -0,0 +1,13 @@
+const BookShelf = () => {
+  return (
+    <>
+      <div>
+        <h3>Add a book</h3>
+        {/* Form goes here */}
+      </div>
+      <div>{/* Book cards displayed here */}</div>
+    </>
+  );
+};
+
+export default BookShelf;
