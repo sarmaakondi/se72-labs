5fa548c75be42693ecae0a1792934337f1ff8fb4
avnms <avnmsarma@gmail.com>
2024-07-04 11:33:27 +1000

    Moved available ingredients to a separate file

--- lifting-state-in-react-lab/src/App.jsx
+++ lifting-state-in-react-lab/src/App.jsx
@@ -1,25 +1,9 @@
 import "./App.css";
 
+import { availableIngredients } from "./ingredients";
 import IngredientList from "./components/IngredientList";
 import BurgerStack from "./components/BurgerStack";
 import { useState } from "react";
-
-export const availableIngredients = [
-  { name: "Kaiser Bun", color: "saddlebrown" },
-  { name: "Sesame Bun", color: "sandybrown" },
-  { name: "Gluten Free Bun", color: "peru" },
-  { name: "Lettuce Wrap", color: "olivedrab" },
-  { name: "Beef Patty", color: "#3F250B" },
-  { name: "Soy Patty", color: "#3F250B" },
-  { name: "Black Bean Patty", color: "#3F250B" },
-  { name: "Chicken Patty", color: "burlywood" },
-  { name: "Lettuce", color: "lawngreen" },
-  { name: "Tomato", color: "tomato" },
-  { name: "Bacon", color: "maroon" },
-  { name: "Onion", color: "lightyellow" },
-  { name: "Cheddar Cheese", color: "#FDE18B" },
-  { name: "Swiss Cheese", color: "#F1E1A8" },
-];
 
 const App = () => {
   const [stack, setStack] = useState([]);
