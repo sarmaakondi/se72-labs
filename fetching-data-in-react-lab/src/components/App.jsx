800d189fff90c93efd75ce854649835e9f613dfa
avnms <avnmsarma@gmail.com>
2024-07-10 18:25:30 +1000

    Implemented dynamic data fetch functionality

--- fetching-data-in-react-lab/src/components/App.jsx
+++ fetching-data-in-react-lab/src/components/App.jsx
@@ -5,26 +5,44 @@ import ItemSearch from "./ItemSearch";
 import ItemList from "./ItemList";
 import * as starWarsDataService from "../services/starWarsDataService";
 
+const categoryList = [
+    "Films",
+    "People",
+    "Planets",
+    "Species",
+    "Starships",
+    "Vehicles",
+];
+
 const dataMapping = {
+    films: ["title", "release_date", "director", "producer"],
+    people: ["name", "gender", "eye_color", "birth_year"],
+    planets: ["name", "population", "diameter", "gravity"],
+    species: ["name", "classification", "designation", "average_lifespan"],
     starships: ["name", "starship_class", "manufacturer", "model"],
+    vehicles: ["name", "vehicle_class", "manufacturer", "model"],
 };
 
 function App() {
     const [starWarsData, setStarWarsData] = useState([]);
     const [searchTerm, setSearchTerm] = useState("");
     const [category, setCategory] = useState("starships");
+    const [isLoading, setIsLoading] = useState(false);
 
     const handleSearch = (event) => {
         setSearchTerm(event.target.value);
     };
 
-    const searchedData = starWarsData.filter((data) =>
-        data.name.toLowerCase().includes(searchTerm.toLowerCase())
-    );
+    const searchedData = searchTerm
+        ? starWarsData.filter((data) =>
+              data.name.toLowerCase().includes(searchTerm.toLowerCase())
+          )
+        : starWarsData;
 
     useEffect(() => {
         const fetchData = async () => {
             try {
+                setIsLoading(true);
                 const data = await starWarsDataService.index(
                     category.toLowerCase()
                 );
@@ -32,6 +50,8 @@ function App() {
                 setStarWarsData(data.results);
             } catch (error) {
                 console.error(error);
+            } finally {
+                setIsLoading(false);
             }
         };
         fetchData();
@@ -41,13 +61,20 @@ function App() {
         <main>
             <div className="header">
                 <h1>Star Wars API</h1>
-                <button
-                    onClick={(event) => setCategory(event.target.innerText)}>
-                    Starships
-                </button>
+                {categoryList.map((item) => (
+                    <button
+                        key={item}
+                        onClick={(event) =>
+                            setCategory(event.target.innerText)
+                        }>
+                        {item}
+                    </button>
+                ))}
             </div>
             <ItemSearch searchTerm={searchTerm} handleSearch={handleSearch} />
             <ItemList
+                category={category}
+                isLoading={isLoading}
                 starWarsData={searchedData}
                 fields={dataMapping[category.toLowerCase()]}
             />
--- fetching-data-in-react-lab/src/components/ItemList.jsx
+++ fetching-data-in-react-lab/src/components/ItemList.jsx
@@ -1,15 +1,25 @@
 import ItemCard from "./ItemCard";
 
-const ItemList = ({ starWarsData, fields }) => {
+const ItemList = ({ category, isLoading, starWarsData, fields }) => {
     return (
         <div className="list">
-            <h2>Starships</h2>
-            <h3>Number of results: {starWarsData.length}</h3>
-            <ul>
-                {starWarsData.map((data) => (
-                    <ItemCard key={data.name} fields={fields} data={data} />
-                ))}
-            </ul>
+            {isLoading ? (
+                <h2>Loading...</h2>
+            ) : (
+                <>
+                    <h2>{category[0].toUpperCase() + category.slice(1)}</h2>
+                    <h3>Number of results: {starWarsData.length}</h3>
+                    <ul>
+                        {starWarsData.map((data) => (
+                            <ItemCard
+                                key={data.name}
+                                fields={fields}
+                                data={data}
+                            />
+                        ))}
+                    </ul>
+                </>
+            )}
         </div>
     );
 };
