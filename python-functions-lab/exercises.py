3b9251208ed8c1afdd6a3bef16cfe50674b984ef
avnms <avnmsarma@gmail.com>
2024-07-23 11:10:41 +1000

    Completed Exercise #5

--- python-functions-lab/exercises.py
+++ python-functions-lab/exercises.py
@@ -75,5 +75,22 @@ def convert_temperature(temperatue, unit):
     return temperatue
 
 
-print("Exercise 4: Convert 0°C to Fahrenheit:", convert_temperature(0, "C"))
-print("Exercise 4: Convert 32°F to Celsius:", convert_temperature(32, "F"))
+# print("Exercise 4: Convert 0°C to Fahrenheit:", convert_temperature(0, "C"))
+# print("Exercise 4: Convert 32°F to Celsius:", convert_temperature(32, "F"))
+
+
+# Exercise 5: Sum to N
+#
+# Write a function named `sum_to` that takes a single integer n and returns the sum of all integers from 1 to n.
+#
+# Examples:
+# sum_to(6) should return 21.
+# sum_to(10) should return 55.
+#
+# Define the function and then call it below.
+def sum_to(num):
+    return (num * (num + 1)) / 2
+
+
+print("Exercise 5:", sum_to(6))
+print("Exercise 5:", sum_to(10))
