c60e6a893183775c2c2ee91880fcb011c6475ef3
avnms <avnmsarma@gmail.com>
2024-07-23 11:19:46 +1000

    Completed Exercise #8

--- python-functions-lab/exercises.py
+++ python-functions-lab/exercises.py
@@ -133,4 +133,27 @@ def calculate_tip(amount, tip):
     return (amount * tip) / 100
 
 
-print("Exercise 7:", calculate_tip(50, 20))
+# print("Exercise 7:", calculate_tip(50, 20))
+
+
+# Exercise 8: Calculate Product of Numbers
+#
+# Write a function named `product` that takes an arbitrary number of numbers, multiplies them, and returns the product.
+# Review your notes on *args for handling an arbitrary number of arguments.
+#
+# Examples:
+# product(-1, 4) should return -4.
+# product(2, 5, 5) should return 50.
+#
+# Define the function and call it with different sets of numbers to test.
+def product(*args):
+    product = 1
+
+    for arg in args:
+        product *= arg
+
+    return product
+
+
+print("Exercise 8:", product(-1, 4))
+print("Exercise 8:", product(2, 5, 5))
