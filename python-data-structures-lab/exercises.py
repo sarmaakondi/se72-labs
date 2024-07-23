be7fdc3ea7e1a69bdf7f9de212791d3e5691a2ae
avnms <avnmsarma@gmail.com>
2024-07-23 11:57:36 +1000

    Completed Exercise #1

--- python-data-structures-lab/exercises.py
+++ python-data-structures-lab/exercises.py
@@ -16,4 +16,23 @@ def example_list_function():
 
 
 # Call the function and print each element
-example_list_function()
+# example_list_function()
+
+
+# Exercise 1: List and Indexing
+#
+# Create a list named students containing at least three student names (strings).
+# Assign the second student’s name to a variable named first_student.
+# Assign the last student’s name to a variable named last_student.
+
+
+def manage_students():
+    students = ["Charlie", "Ben", "Sarma"]
+    first_student = students[1]
+    last_student = students[-1]
+
+    return first_student, last_student
+
+
+# Call the function and print the result
+print("Exercise 1:", manage_students())
