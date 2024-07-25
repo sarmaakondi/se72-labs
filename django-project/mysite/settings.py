b8bc2d9013d062334c0ea49eb0ca9cdcaf004645
avnms <avnmsarma@gmail.com>
2024-07-25 19:53:18 +1000

    Add DB connection details and validate

--- django-project/mysite/settings.py
+++ django-project/mysite/settings.py
@@ -68,8 +68,12 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "polls",
+        "USER": os.environ.get("USER"),
+        "PASSWORD": os.environ.get("PASSWORD"),
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
